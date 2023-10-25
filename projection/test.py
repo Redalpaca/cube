98a1986d06c08b2c93f037697fc95ce02ff8557b
Redalpaca <zhoualpaca@gmail.com>
2023-10-25 20:16:50 +0800

    project test

--- projection/test.py
+++ projection/test.py
@@ -10,9 +10,9 @@ import sys
    z/   |y
 """
 
-numLine = 40
+numLine = 30
 x_max = numLine*2
-y_max = numLine*4
+y_max = numLine*5
 x_bias = numLine
 y_bias = numLine*2
 
@@ -23,16 +23,18 @@ y_bias = numLine*3
 
 theta = np.pi / 16
 
-def project_yz(p0:tuple, n_0:tuple, n_1:tuple, bias:tuple):
-    x0, y0, z0 = p0[0], p0[1], p0[2]
-    m, n, s = n_0[0], n_0[1], n_0[2]
-    m1, n1, s1 = n_1[0], n_1[1], n_1[2]
-    a, b, c = bias[0], bias[1], bias[2]
-    t = -( m*(x0-a) )/(m*m1)
-    x1, y1, z1 = m*t+x0, m*t+y0, m*t+z0
-    return (x1, y1, z1)
-
 def project(p0:tuple, n_0:tuple, n_1:tuple, bias:tuple):
+    """_summary_
+
+    Args:
+        p0 (tuple):     coordinate of the point.
+        n_0 (tuple):    normal vectior of the projection plane
+        n_1 (tuple):    project vector
+        bias (tuple):   reference point coordinate of the projection plane
+
+    Returns:
+        _type_: _description_
+    """
     x0, y0, z0 = p0[0], p0[1], p0[2]
     m, n, s = n_0[0], n_0[1], n_0[2]
     m1, n1, s1 = n_1[0], n_1[1], n_1[2]
@@ -69,6 +71,8 @@ class Cube(object):
         self.coordinate_xy_1 = self.__init_coordinate_xy__(-refer, -refer, -refer, numLine= numLine)
         
         
+        
+        
         # diagnose
         self.normal_vector = np.array([[-1,0,0],
                                        [0,-1,0],
@@ -78,17 +82,40 @@ class Cube(object):
                                        [0,0,-1]])
         self.normal_vector_ball = np.array([ [c[0]+numLine/2, c[1], c[2] ] for c in self.coordinate_yz])
         self.visual_vector = np.array([0,0,1])
+        
+        self.disappoint_vec = (0, 0, -100)
+        
+        # 投影面的坐标及参考点 
         self.ground = [(0, 0, 1), (0, 0, -numLine)]
+        self.ground = [(0, 0, 1), (0, 0, 0)]
         # self.ground = [(1, 0, 0), (numLine//2, 0, 0)]
         # self.ground = [(0, 1, 0), (0, numLine, 0)]
         
         self.squares = [self.coordinate_yz, self.coordinate_xz, self.coordinate_xy,
                         self.coordinate_yz_1, self.coordinate_xz_1, self.coordinate_xy_1]
+        
+        self.temp = []
+        for square in self.squares[1:]:
+            self.temp.append( square + (0, 1.25*numLine, -1.25*numLine) )
+        self.temp.append(self.__init_coordinate_yz__(-refer, -refer, refer, numLine= numLine) + (0, 1.25*numLine, -1.25*numLine))
+        
+        self.squares.extend(self.temp)
+        
         # self.squares = [self.coordinate_yz, self.coordinate_xz, self.coordinate_xy]
         self.lightMap = [' ', '.', '*', '%']
         self.lightMap = " .-:;=+*#%@@@@@@"
         self.lightMap_ball = " .--:;=+**##%@@"
         pass
+    
+    def __init_yz__(self, x_0, y_0, z_0, numLine):
+        return np.array( [[(x_0, y_0 + i, z_0 + j) for i in range(-numLine//2 ,numLine//2)] for j in range(-numLine//2, numLine//2)],
+                            dtype= np.float64 ).reshape(numLine*numLine, 3)
+    def __init_xz__(self, x_0, y_0, z_0, numLine):
+        return np.array( [[(x_0 + i, y_0, z_0 + j) for i in range(-numLine//2 ,numLine//2)] for j in range(-numLine//2, numLine//2)],
+                            dtype= np.float64 ).reshape(numLine*numLine, 3)
+    def __init_xy__(self, x_0, y_0, z_0, numLine):
+        return np.array( [[(x_0 + i, y_0 + j, z_0) for i in range(-numLine//2 ,numLine//2)] for j in range(-numLine//2, numLine//2)],
+                            dtype= np.float64 ).reshape(numLine*numLine, 3)
     
     def __init_coordinate_yz__(self, x_0, y_0, z_0, numLine):
         return np.array( [[(x_0, y_0 + i, z_0 - j) for i in range(numLine)] for j in range(numLine)],
@@ -150,20 +177,32 @@ class Cube(object):
         
         def square2buf(cube: Cube, square, light, x_bias, y_bias):
             for c in square:
-                cube.buf[int(c[0]+x_bias)%row, int(2*c[1]+y_bias)%col] = light
+                project_vec = c - self.disappoint_vec
+                c_ = project(c, self.ground[0], project_vec, self.ground[1])
+                x = int(c_[0]+x_bias)%row
+                y = int(2*c_[1]+y_bias)%col
+                cube.buf[x, y] = light
         
         # 投影
-        for i, square in enumerate(self.squares):
-            for c in square:
-                shadow = project(c, self.ground[0], -self.illuminant_vector, self.ground[1])
-                if (0 <= shadow[0]+x_bias < row-1) and (0 <= 2*shadow[1]+y_bias < col-1):
-                    cube.buf[int(shadow[0]+x_bias), int(2*shadow[1]+y_bias)] = 1
-                pass
+        # for i, square in enumerate(self.squares):
+        #     for c in square:
+        #         shadow = project(c, self.ground[0], -self.illuminant_vector, self.ground[1])
+        #         if (0 <= shadow[0]+x_bias < row-1) and (0 <= 2*shadow[1]+y_bias < col-1):
+        #             cube.buf[int(shadow[0]+x_bias), int(2*shadow[1]+y_bias)] = 1
+        #         pass
         
-        for i, square in enumerate(self.squares):
+        for i, square in enumerate(self.squares[:6]):
             # 依照投影向量与各个面法向量的点积, 决定当前应该显示哪个面
             if dot_product[i] > 0:
                 square2buf(self, square, light_product[i], x_bias, y_bias)  
+        
+        for i, square in enumerate(self.squares[6:]):
+            # 依照投影向量与各个面法向量的点积, 决定当前应该显示哪个面
+            # if dot_product[i] > 0:
+            square2buf(self, square, light_product[i], x_bias, y_bias)  
+        # for i, square in enumerate(self.temp):
+        #     # 依照投影向量与各个面法向量的点积, 决定当前应该显示哪个面
+        #     square2buf(self, square, light_product[i], x_bias, y_bias)  
                 
         # 单独处理那个球
         for i, c in enumerate(self.squares[0]):
